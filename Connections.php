25de8f075831e5fb455ed7755c21fcf0c6f429c6
LordAldren <lordaldren.silvano07@gmail.com>
2025-09-27 16:15:13 +0800

    .

--- Connections.php
+++ Connections.php
@@ -6,13 +6,13 @@ $password = '123';     // your MySQL password
 
 try {
     // Create PDO instance
-    $Connections = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
     // Set PDO error mode to Exception
-    $Connections->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Optional: fetch assoc by default
-    $Connections->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
     // You can log the error instead of echoing in production
