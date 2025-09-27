cb5539140f04f5e23a56824310711c109e19d964
LordAldren <lordaldren.silvano07@gmail.com>
2025-09-27 23:56:51 +0800

    .

--- Interviewschedule.php
+++ Interviewschedule.php
@@ -223,31 +223,31 @@ try {
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                     <div>
-                        <label class="block text-white text-sm font-medium  mb-1">Position*</label>
+                        <label class="block text-white text-sm font-medium text-white-700  mb-1">Position*</label>
                         <input type="text" name="position" required class="w-full px-3 py-2 border border-gray-300 rounded-lg">
                     </div>
                     <div>
-                        <label class="block #d37a15text-white  text-sm font-medium  mb-1">Interviewer*</label>
+                        <label class="block text-white  text-sm font-medium text-white-700  mb-1">Interviewer*</label>
                         <input type="text" name="interviewer" required class="w-full px-3 py-2 border border-gray-300 rounded-lg">
                     </div>
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                     <div>
-                        <label class="block text-white text-sm font-medium text-gray-700 mb-1">Start Time*</label>
+                        <label class="block  text-sm font-medium text-white-700 mb-1">Start Time*</label>
                         <input type="datetime-local" name="start_time" required class="w-full px-3 py-2 border border-gray-300 rounded-lg">
                     </div>
                     <div>
-                        <label class="block text-white text-sm font-medium text-gray-700 mb-1">End Time*</label>
+                        <label class="block text-white text-sm font-medium text-white-700 mb-1">End Time*</label>
                         <input type="datetime-local" name="end_time" required class="w-full px-3 py-2 border border-gray-300 rounded-lg">
                     </div>
                 </div>
                  <div class="mb-4">
-                    <label class="block text-white text-sm font-medium text-gray-700 mb-1">Location*</label>
+                    <label class="block text-white text-sm font-medium text-white-700 mb-1">Location*</label>
                     <input type="text" name="location" required class="w-full px-3 py-2 border border-gray-300 rounded-lg">
                 </div>
                 <div class="flex justify-end gap-3">
-                    <button type="button" id="cancelBtn" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300">Cancel</button>
-                    <button type="submit" class="px-4 py-2 bg-brand-500 text-white rounded-lg hover:bg-brand-600" style="background-color: green;" >Save Schedule</button>
+                    <button type="button" id="cancelBtn" class="px-4 py-2 text-white-700 bg-gray-200 rounded-lg hover:bg-gray-300">Cancel</button>
+                    <button type="submit" class="px-4 py-2 bg-brand-500 text-white-700 rounded-lg hover:bg-brand-600" style="background-color: green;" >Save Schedule</button>
                 </div>
             </form>
         </div>
